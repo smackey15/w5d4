2a1d95aba6a64fa7b1a7fab039cc4511daf84d5a
egumin <ethangumin@gmail.com>
2022-01-06 17:35:24 -0500

    finish first migration and create User model

---
+++ URLShortener/app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+class User < ApplicationRecord
+end
