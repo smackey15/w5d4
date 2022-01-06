2ceec5d358399e73620f87003feb328fed44278b
egumin <ethangumin@gmail.com>
2022-01-06 16:46:27 -0500

    complete Associations Exercise

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -24,7 +24,8 @@ class Course < ApplicationRecord
         foreign_key: :prereq_id,
         class_name: :Course
 
-    has_one :instructor,
-        through: :enrollments,
-        source: :user 
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 end
